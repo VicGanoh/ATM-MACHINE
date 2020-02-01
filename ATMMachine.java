e829587f0d7e8b54e826b6e8d7771b374c3397e9
Victor Tetteh Ganoh Ahumah <52656133+VicGanoh@users.noreply.github.com>
2020-02-01 21:26:14 +0000

    Add files via upload

---
+++ ATMMachine.java
@@ -0,0 +1,212 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
+import java.util.Scanner;
+
+public class ATMMachine {
+    //global variable for initial amount
+    public static final double init_balance = 1000;
+
+    //global variable for current amount/amount update
+    public static double current_amount;
+    //global variable for account number
+    public static int acct_no;
+
+    public static void main(String[] args) {
+       // accessing the menu
+       TopMenu();
+
+       //select an input for the menu
+        System.out.println("Please select an option: ");
+        Scanner input = new Scanner(System.in);
+        int menu_input = input.nextInt();
+
+        switch (menu_input){
+            case 1:
+                //SignIn
+                System.out.println("Enter your account number: ");
+                Scanner input1 = new Scanner(System.in);
+                acct_no = input1.nextInt();
+
+                //assume account numbers are from (0...9)
+                //validate input
+                int counter = 1;
+                while(acct_no<0 || acct_no>9){
+                    System.out.println("Number is not valid!!!");
+                    System.out.println();
+
+                    /**
+                     * menu is displayed again @TopMenu when acount number  is invalid
+                     */
+                    TopMenu();
+
+                    System.out.println("Please select an option: ");
+                    menu_input = input.nextInt();
+                    System.out.println("Enter your account number: ");
+                    acct_no = input1.nextInt();
+                    counter++;
+                }
+        //ACCESS MENU AFTER SIGNING IN
+                TopMenu_2();
+
+
+                break;
+            case 2:
+                //BALANCE OPTION
+            case 3:
+                //DEPOSIT OPTION
+            case 4:
+                //WITHDRAW OPTION
+                System.out.println("Please sign in first.");
+                break;
+            case 5:
+                //EXIT OPTION
+                System.out.println("Goodbye");
+                break;
+            default:
+                if (menu_input<1 || menu_input>5)
+                    System.out.println("Number out of range!");
+             break;
+        }
+
+
+    }
+
+
+
+
+//Funtions/Methods
+
+    //This displays The Menu
+    public static void TopMenu(){
+        System.out.println("Welcome to the ATM Machine.");
+        System.out.println("1.Sign In");
+        System.out.println("2.Balance");
+        System.out.println("3.Deposit");
+        System.out.println("4.Withdraw");
+        System.out.println("5.Exit");
+
+    }
+
+    //This Displays Menu After Signed In
+    public static void TopMenu_2(){
+        System.out.println("Please select an option: ");
+        System.out.println("1.Balance");
+        System.out.println("2.Deposit");
+        System.out.println("3.Withdraw");
+        System.out.println("4.Exit");
+
+        Scanner input = new Scanner(System.in);
+        int menu_input = input.nextInt();
+
+        switch (menu_input){
+            case 1:
+                //Balance check
+                Bal();
+                break;
+            case 2:
+                //Deposit
+                Deposit_();
+                break;
+            case 3:
+                //Withdrawal
+                Withdrawl_();
+                break;
+            case 4:
+                exit_();
+                break;
+            default:
+                System.out.println("Invalid Entry.");
+                System.out.print("Please try again.");
+                break;
+        }
+
+
+    }
+
+
+    //Using an Aray to Hold Account Balance
+   /* public static void AcBal(){
+        int size = 10;
+        //double init_balance = 1000;
+        double acctbal[] = new double[size];
+        for(int i = 0 ; i<size; i++){
+            acctbal[i] = init_balance;
+            System.out.println(init_balance);
+        }
+    }*/
+
+    /**Balance option @Bal**/
+    public static void Bal(){
+        int size = 10;
+        // double init_balance = 1000;
+        double acctbal[] = new double[size];
+
+        for(int i = 0 ; i<size; i++){
+            acctbal[i] = init_balance;
+            acctbal[acct_no] = init_balance;
+           // System.out.println(init_balance);
+        }
+
+        System.out.println("Available Balance:$" + init_balance);
+        System.out.println("Updated Balance: " + current_amount);
+
+        //Balance checking
+        if (init_balance<100)
+            System.out.println("Low Balance.");
+        System.out.println(); 
+    }
+
+
+    /**Deposit option @Deposit_**/
+    public static void Deposit_(){
+        //prompt user to enter amount
+        System.out.print("Please enter an amount: ");
+        Scanner input = new Scanner(System.in);
+        int amount = input.nextInt();
+
+        //verify if input is positive
+        while(amount<1){
+            System.out.println("Invalid amount.\nTry again.");
+            System.out.print("Please enter an amount: ");
+            amount = input.nextInt();
+            TopMenu_2();
+        }
+
+        //update of current balance
+       current_amount = amount + init_balance;
+        System.out.println("Your current balance: $" + current_amount);
+    }
+
+    /**Withdrawal option @Withdrawl_**/
+    public static void Withdrawl_(){
+
+        //prompt user to enter amount to withdrawl
+       // System.out.print("Enter an amount: ");
+
+        //int amount = input.nextInt();
+        if (init_balance>0)
+            System.out.println("Your balance is sufficient.");
+        else
+            System.out.println("Your balance is insuffiencient.");
+
+            Bal();
+        System.out.println("Enter an amount: ");
+        Scanner input = new Scanner(System.in);
+        int amount = input.nextInt();
+
+
+        //calculates and displays current balance after withdrawal
+        current_amount = init_balance - amount;
+        System.out.println("Your available balance is: $" + current_amount);
+    }
+
+    /**exit option for user **/
+    public static void exit_(){
+        System.out.println("Goodbye.");
+        System.out.println();
+        TopMenu();
+    }
+}
+
+
+
